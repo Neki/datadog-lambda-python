57cc2404b7d2d8ee5ff7791f41f0036aabd13d0c
Benoît Faucon <benoit.faucon@terality.com>
2021-09-08 09:38:55 +0200

    chore: update ddtrace version to 0.52.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         "aws-xray-sdk==2.8.0",
         "datadog==0.41.0",
-        "ddtrace==0.50.0",
+        "ddtrace==0.52.1",
         "wrapt==1.11.2",
         # If building for Python 3, use the latest version of setuptools
         "setuptools>=54.2.0; python_version >= '3.0'",
